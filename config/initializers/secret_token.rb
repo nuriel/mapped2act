e84e39e7348b2e2c31dbe7247704ce2995bad9ee
nuriel <nuriel98@gmail.com>
2013-09-03 13:27:46 +0300

    initial rails installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapped2act::Application.config.secret_token = 'ee5ff66ebc86ec8a799bbeda4a5b4887bda6cbfe6534b4deeefac220e94ec47e62233dbf269216139093999db871c01c457f216b24590e7c56dfd0ffca2eb499'
