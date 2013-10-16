bfee8f080328c0e9e91fb9d206d1147e8ca0de37
nuriel <nuriel98@gmail.com>
2013-10-16 12:50:05 -0700

    edit cause location automcomplete

--- app/serializers/cause_serializer.rb
+++ app/serializers/cause_serializer.rb
@@ -1,4 +1,4 @@
-class CauseSerializer < ActiveModel::Serializer
-  attributes :address, :email, :latitude, :logo_url, :longitude, :name, :one_liner, :phone, :social_contribution,
-             :website
-end
+#class CauseSerializer < ActiveModel::Serializer
+#  attributes :address, :email, :latitude, :logo_url, :longitude, :name, :one_liner, :phone, :social_contribution,
+#             :website
+#end
